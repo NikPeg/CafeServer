bbf64a84d55e4ac28eac02908b359f94c436cff4
NikPeg <peganov.nik@gmail.com>
2025-01-03 12:43:25 +0300

    assert

--- client_test.go
+++ client_test.go
@@ -4,6 +4,8 @@ import (
     "net/http"
     "net/http/httptest"
     "testing"
+    "github.com/stretchr/testify/require"
+    "github.com/stretchr/testify/assert"
 )
 
 func TestPostalCode(t *testing.T) {
@@ -34,4 +36,27 @@ func TestPostalCode(t *testing.T) {
     if actualAddress != address {
         t.Errorf("expected address %s, got %s", address, actualAddress)
     }
+}
+
+func TestPostalCodeWithAssert(t *testing.T) {
+    address := "Омск, п. Верхние пупки, д. 6"
+    postalCode := "100000"
+
+    var actualAddress string
+    var actualPostalCode string
+
+    server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+        actualAddress = req.URL.Query().Get("address")
+
+        rw.Write([]byte(postalCode))
+    }))
+
+    defer server.Close()
+
+    postalCodeClient := PostalCodeClient{server.Client(), server.URL}
+    actualPostalCode, err := postalCodeClient.PostalCode(address)
+
+    require.NoError(t, err)
+    assert.Equal(t, postalCode, actualPostalCode)
+    assert.Equal(t, address, actualAddress)
 }
